7f71c45ab908dee0f1d4387ebe9413f002326de7
riteme <1412803389@qq.com>
2016-05-05 21:41:29 +0800

    Completed MemoryWatcher

--- src/judge2.py
+++ src/judge2.py
@@ -8,6 +8,23 @@ import os
 import sys
 import time
 
+import threading
+
+
+def info(message):
+    print("\033[32m(info)\033[0m {}".format(message))
+
+def warning(message):
+    print("\033[33m(warn)\033[0m {}".format(message))
+
+def error(message):
+    print("\033[31m(error)\033[0m {}".format(message))
+
+try:
+    import psutil
+except:
+    warning("psutil not installed. Memory states will not be watched.")
+
 
 class Compiler(object):
     """Represent a compiler"""
@@ -40,15 +57,63 @@ class Timer(object):
     def __init__(self):
         super(Timer, self).__init__()
         self.current = time.time()
-    
+
     def restart(self):
         self.current = time.time()
 
     def tick(self):
         now = time.time()
-        span = int((now - self.current) * 1000)
+        span = now - self.current
 
         return span
+
+
+class MemoryWatcher(object):
+    """Watch for memory usage"""
+    def __init__(self, timespan=0.016):
+        super(MemoryWatcher, self).__init__()
+        self.pid = 0
+        self.max = 0.0
+        self.timespan = timespan
+
+    def _main(self):
+        try:
+            proc = psutil.Process(self.pid)
+        except:
+            warning("Memory watcher will not work.")
+            return
+
+        while self._flag:
+            try:
+                meminfo = proc.memory_info()
+                self.max = max(self.max, meminfo.vms)
+
+                time.sleep(self.timespan)
+
+            except:
+                return
+
+    def start(self, pid):
+        self.pid = pid
+        self._flag = True
+        self._thread = threading.Thread(
+            target = MemoryWatcher._main,
+            name = "Memory Watcher",
+            args = (self, )
+        )
+        self._thread.start()
+
+    def stop(self, wait=True):
+        self._flag = False
+
+        if wait:
+            self._thread.join()
+
+    def get_history_max(self):
+        return self.max
+
+    def reset(self):
+        self.max = 0.0
 
 if __name__ == "__main__":
     t = Timer()
